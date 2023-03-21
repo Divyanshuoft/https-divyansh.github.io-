b9ba7730ff3ae59c6f6c331d7b2d0e7f6843ebdc
Divyanshuoft <114203084+Divyanshuoft@users.noreply.github.com>
2023-03-21 13:30:06 -0400

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -109,6 +109,21 @@ function myFunction() {
       moreText.style.display = "inline";
     }
   }
+  function myFunction4() {
+    var dots = document.getElementById("dots4");
+    var moreText = document.getElementById("more4");
+    var btnText = document.getElementById("myBtn4");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
 
 window.onload = function() {
     var elements = document.getElementsByClassName('typewrite');
